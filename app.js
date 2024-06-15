c824912b9c57995ac28fc6a67a2f28477d2737ff
michael <michaelblossom8654@gmail.com>
2024-06-15 13:20:21 +0530

    ci:setup relative path

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 
@@ -10,7 +11,8 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json({ limit: "10kb" }));
 
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res, next) => {
   res.send("Hello from middleware");
